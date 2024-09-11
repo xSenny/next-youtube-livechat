66da660230e04afd9cedc0ef0db59a814dcc623e
xSenny <ursuarsenie@gmail.com>
2024-09-11 19:12:59 +0300

    Changes

--- apps/web/app/[...url]/page.tsx
+++ apps/web/app/[...url]/page.tsx
@@ -6,7 +6,7 @@ export default function Home({params}: {params: {url: string[]}}) {
   return (
     <main className='z-10 flex min-h-dvh flex-col items-center bg-[#282a36] p-4 text-gray-200'>
       <AppCreditSection />
-      <Demo parsedUrl={params.url.join('/')}/>
+      <Demo parsedUrl={`https:/www.youtube.com/watch?v=${params.url.join('/')}`}/>
     </main>
   );
 }
